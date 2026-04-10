07f938adab90b9c63b3054965f622e66eddf5ee9
saketh <sakethvijayagiri123@gmail.com>
2026-04-10 10:35:38 +0000

    Test webhook trigger

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -1,7 +1,37 @@
 package com.devops;
 
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class App {
     public int add(int a, int b) {
         return a + b;
+    }
+
+    public static void main(String[] args) throws IOException {
+        int port = 9090;
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        server.createContext("/", new HttpHandler() {
+            public void handle(HttpExchange exchange) throws IOException {
+                String response = "<html><body>" +
+                    "<h1>Student App is Running!</h1>" +
+                    "<p>CI/CD Pipeline: Jenkins + Docker + Maven</p>" +
+                    "<p>2 + 3 = " + new App().add(2, 3) + "</p>" +
+                    "</body></html>";
+                exchange.getResponseHeaders().set("Content-Type", "text/html");
+                exchange.sendResponseHeaders(200, response.length());
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+        });
+
+        server.start();
+        System.out.println("Server started at http://localhost:" + port);
     }
 }
