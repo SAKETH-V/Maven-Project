7869fca37f55b4b03623c7e9566b3796ce4e4d5b
saketh <sakethvijayagiri123@gmail.com>
2026-04-10 11:02:27 +0000

    Test 1

--- src/main/java/com/devops/BrokenClass.java
+++
@@ -1,8 +0,0 @@
-package com.devops;
-
-public class BrokenClass {
-    public void doSomething() {
-        String message = "Hello"    // missing semicolon
-        int number = "not a number" // wrong type - string assigned to int
-    }
-}
